682c0ab9336ec93a1ab51d85dc1b13be5ffa8d65
Alvaro Toro <alvaro_toro22@hotmail.com>
2023-05-22 19:46:35 -0500

    juega un poco

---
+++ js/codigo.js
@@ -0,0 +1,42 @@
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function eleccion(jugada) {
+    let resultado = ""
+    if(jugada == 1) {
+       resultado = "Piedra 🪨"
+    } else if(jugada == 2) {
+        resultado = "Papel 📰"
+    } else if(jugada == 3) {
+        resultado = "tijera ✂️"
+    } else {
+        resultado = "MALA ELECCION" 
+    }
+    return resultado
+}
+// 1 es piedra, 2 es papel, 3 es tijera
+let jugador = 0
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+while (triunfos < 3 && perdidas < 3) {
+     pc = aleatorio(1,3)
+    jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
+    //alert("Elegiste " + jugador)
+    alert("Tu eliges: " + eleccion(jugador))
+    alert("PC elige: " + eleccion(pc))
+
+    // COMBATE
+    if(pc == jugador) {
+        alert("EMPATE")
+    } else if((jugador == 1 && pc == 3)
+ || (jugador == 2 && pc == 1) ||
+    (jugador == 3 && pc == 2)) {
+        alert("GANASTE") 
+        triunfos++ 
+    } else {
+        alert("PERDISTE")
+        perdidas++ 
+    }
+}
+    alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces.")
