4002803b1940b87a1f20a912611ce3e9fd6e935d
Alvaro Toro <alvaro_toro22@hotmail.com>
2023-05-30 15:37:26 -0500

    asi vamos

--- NewGame/js/catWar.js
+++ NewGame/js/catWar.js
@@ -1,41 +1,86 @@
+let ataqueJugador
+let ataqueEnemigo
 
-function iniciarJuego(){
-    let botonAvatarJugador = document.getElementById('seleccionar-avatar')
-    botonAvatarJugador.addEventListener('click' , seleccionarAvatarJugador)
+function iniciarJuego() {
+   let botonAvatarJugador = document.getElementById('seleccionar-avatar')
+   botonAvatarJugador.addEventListener('click', seleccionarAvatarJugador)
+
+
+   let botonRoca = document.getElementById('seleccionar-roca')
+   botonRoca.addEventListener('click', ataqueRoca)
+   let botonRayo = document.getElementById('seleccionar-rayo')
+   botonRayo.addEventListener('click', ataqueRayo)
+   let botonAgua = document.getElementById('seleccionar-agua')
+   botonAgua.addEventListener('click', ataqueAgua)
+
 }
 
-function seleccionarAvatarJugador(){
-    let inputCatrock = document.getElementById('Catrock')
-    let inputLightningcat = document.getElementById('Lightningcat')
-    let inputCatoola = document.getElementById('Catoola')
-    let botonseleccionaravatar = document.getElementById('seleccionar-avatar')
-    let spanavatarjugador = document.getElementById('avatar-jugador')
+function seleccionarAvatarJugador() {
+   let inputCatrock = document.getElementById('Catrock')
+   let inputLightningcat = document.getElementById('Lightningcat')
+   let inputCatoola = document.getElementById('Catoola')
+   let botonseleccionaravatar = document.getElementById('seleccionar-avatar')
+   let spanavatarjugador = document.getElementById('avatar-jugador')
 
-     if(inputCatrock.checked){
-        spanavatarjugador.innerHTML = 'Catrock'
-     }else if(inputLightningcat.checked){
+   if (inputCatrock.checked) {
+      spanavatarjugador.innerHTML = 'Catrock'
+   } else if (inputLightningcat.checked) {
       spanavatarjugador.innerHTML = 'Lightningcat'
-     }else if(inputCatoola.checked){
+   } else if (inputCatoola.checked) {
       spanavatarjugador.innerHTML = 'Catoola'
-     }else {
-        alert('Selecciona avartar')
-     }
-         
-     seleccionarAvatarEnemigo()
+   } else {
+      alert('Selecciona avartar')
+   }
+
+   seleccionarAvatarEnemigo()
 }
 
-function seleccionarAvatarEnemigo(){
-   let avatarAleatorio= aleatorio(1,3)
-   let spanAvatarEnemigo= document.getElementById('Avatar-Enemigo')
+function seleccionarAvatarEnemigo() {
+   let avatarAleatorio = aleatorio(1, 3)
+   let spanAvatarEnemigo = document.getElementById('Avatar-Enemigo')
 
-      if(avatarAleatorio == 1) {
+   if (avatarAleatorio == 1) {
       spanAvatarEnemigo.innerHTML = 'Catrock'
-   } else if(avatarAleatorio == 2) {
+   } else if (avatarAleatorio == 2) {
       spanAvatarEnemigo.innerHTML = 'Lightningcat'
-   } else { 
+   } else {
       spanAvatarEnemigo.innerHTML = 'Catoola'
    }
 }
+
+function ataqueRoca() {
+   ataqueJugador = 'ROCA'
+   ataqueAleatorioEnemigo()
+}
+function ataqueRayo() {
+   ataqueJugador = 'RAYO'
+   ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+   ataqueJugador = 'AGUA'
+   ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+   let ataqueAleatorio = aleatorio(1,3)
+   if(ataqueAleatorio == 1){
+      ataqueEnemigo = 'ROCA'
+   }else if(ataqueAleatorio == 2){
+         ataqueEnemigo = 'RAYO'
+      } else {
+         ataqueEnemigo = 'AGUA'
+      } 
+      crearMensaje()
+   }
+
+function crearMensaje(){
+
+   let sectionMensaje = document.getElementById('mensajes')
+   let parrafo = document.createElement('p')
+   parrafo.innerHTML = 'Tu atacaste con ' + ataqueJugador + ' el enemigo ataco con ' +  ataqueEnemigo 
+   sectionMensaje.appendChild(parrafo) 
+
+} 
 
 function aleatorio(min, max) {
    return Math.floor(Math.random() * (max - min + 1) + min)
